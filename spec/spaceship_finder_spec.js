e720b7297df59de5da0192183ce42f5e9e923f91
Julia Pabst <ju.pabst@gmail.com>
2024-05-15 18:01:37 +0200

    adjust tests to fit the real task

--- spec/spaceship_finder_spec.js
+++ spec/spaceship_finder_spec.js
@@ -3,20 +3,20 @@ describe("Spaceship finder", function () {
     spaceship = new spaceShipFinder();
   });
 
-  it("should return [0, 0] if it is on the first field", function () {
-    expect(spaceship.find("X..\n...\n...")).toEqual([0, 0]);
+  it("should return [2, 0] if it is on the first field", function () {
+    expect(spaceship.find("X..\n...\n...")).toEqual([2, 0]);
   });
 
-  it("should return [3, 3] if it is on the last field in a small field", function () {
-    expect(spaceship.find("...\n...\n..X")).toEqual([2, 2]);
+  it("should return [2, 0] if it is on the last field in a small field", function () {
+    expect(spaceship.find("...\n...\n..X")).toEqual([2, 0]);
   });
 
-  it("should return [0, 2] if it is on the third field in the first row", function () {
-    expect(spaceship.find("..X\n...\n...")).toEqual([0, 2]);
+  it("should return [2, 2] if it is on the third field in the first row", function () {
+    expect(spaceship.find("..X\n...\n...")).toEqual([2, 2]);
   });
 
-  it("should return [1, 0] if it is on the first field in the second row", function () {
-    expect(spaceship.find("...\nX..\n...")).toEqual([1, 0]);
+  it("should return [0, 1] if it is on the first field in the second row", function () {
+    expect(spaceship.find("...\nX..\n...")).toEqual([0, 1]);
   });
 
   it("should return 'Spaceship lost forever.' if there is no X", function () {

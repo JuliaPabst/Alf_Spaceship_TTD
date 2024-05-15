3f161215bd0ad9b751070109cb26a82c318b0132
Julia Pabst <ju.pabst@gmail.com>
2024-05-15 17:46:07 +0200

    Fix test 3

--- src/spaceship_finder.js
+++ src/spaceship_finder.js
@@ -3,11 +3,23 @@ function spaceShipFinder(map) {}
 spaceShipFinder.prototype.find = function (map) {
   let position;
 
-  if (map[0] === "X") {
+  let mapArrays = map.split("\n");
+
+  console.log(mapArrays);
+
+  for (let i = 0; i < mapArrays.length; i++) {
+    if (mapArrays[0][i] == "X") {
+      position = [0, i];
+    }
+  }
+
+  if (mapArrays[0][0] === "X") {
     position = [0, 0];
-  } else {
+  } else if (mapArrays[2][2] === "X") {
     position = [2, 2];
   }
+
+  console.log(position);
 
   return position;
 };

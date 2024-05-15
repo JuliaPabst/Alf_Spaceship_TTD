32e90d08a75592dbbae743b72e387338832bb852
Julia Pabst <ju.pabst@gmail.com>
2024-05-15 18:13:42 +0200

    refactor everything to finish the project

--- src/spaceship_finder.js
+++ src/spaceship_finder.js
@@ -3,13 +3,11 @@ function spaceShipFinder(map) {}
 spaceShipFinder.prototype.find = function (map) {
   let position;
   let mapArrays = map.split("\n");
-  console.log(mapArrays);
 
   for (let i = mapArrays.length - 1; i >= 0; i--) {
     for (let j = 0; j < mapArrays[i].length; j++) {
       if (mapArrays[i][j] === "X") {
-        position = [j, mapArrays.length - 1 - i]; // Adjust for the bottom-left origin
-        console.log(position);
+        position = [j, mapArrays.length - 1 - i];
       }
     }
   }
